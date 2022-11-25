6343069ea41c59d9454d57b91ce4f81c74c4126c
Kirill Kulikov <kikulikov@@users.noreply.github.com>
2022-11-25 13:50:26 +0000

    metro

--- cckeys.sh
+++ cckeys.sh
@@ -4,36 +4,33 @@
 # 2. Make sure that you are logged in to Confluent Cloud `confluent login`
 
 # Azure configuration parameters
-az_vault_name="kirill-kulikov-keyvault"
-az_vault_apikey_name="confluent-cloud-api-key"
-az_vault_apisecret_name="confluent-cloud-api-secret"
+az_subscrition="a23ea812-1d8a-41f1-84b2-806938e8e2e6"
+az_keyvault_name="kirill-kulikov-keyvault"
+az_keyvault_apikey_name=$1 #"confluent-cloud-api-key"
+az_keyvault_apisecret_name=$2 #"confluent-cloud-api-secret"
 
 # Confluent Cloud configuration parameters
-resource="lkc-57d13z"
+resource=$3 #"lkc-57d13z"
 
-timeback=10
-today=$(date)
-before="$(date -d "$today - $timeback hours" +%s)"
+echo "creating a new API Key and Secret"
+json=$(confluent api-key create --resource "$resource" \
+--description "Metro $(date +%Y-%m-%d-%H-%M-%S)" --output json)
 
-# counts a number of API keys which are near to expire
-count=$(confluent api-key list --output json --resource $resource \
-| jq "map(select(.created | fromdateiso8601 < $before)) | length")
+echo $json | jq '.'
 
-if [ $count -gt 0 ];
-then
-    echo "creating a new API Key and Secret"
-    json=$(confluent api-key create --resource "$resource" \
-    --description "Metro $(date +%Y-%m-%d-%H-%M-%S)" --output json)
+key=$(echo $json | jq -r '.key')
+secret=$(echo $json | jq -r '.secret')
 
-    echo $json | jq '.'
+az keyvault secret set --vault-name $az_keyvault_name \
+--name $az_keyvault_apikey_name --value $key --subscription $az_subscrition
 
-    key=$(echo $json | jq -r '.key')
-    secret=$(echo $json | jq -r '.secret')
+az keyvault secret set --vault-name $az_keyvault_name \
+--name $az_keyvault_apisecret_name --value $secret --subscription $az_subscrition
 
-    az keyvault secret set --vault-name $az_vault_name --name $az_vault_apikey_name --value $key
-    az keyvault secret set --vault-name $az_vault_name --name $az_vault_apisecret_name --value $secret
-fi
-
-# az account set -s a23ea812-1d8a-41f1-84b2-806938e8e2e6
-# az group list --subscription a23ea812-1d8a-41f1-84b2-806938e8e2e6
-# az keyvault list --resource-group kirill-kulikov-resourcegroup
+# Script flow
+# 1. Creates a new key/secret pair in Confluent Cloud
+# 2. The script does not update/delete old keys/secrets in Confluent Cloud
+# 3. So all applications still using the old key/secret can continue working without discruption
+# 4. Finally, the script UPDATES the key/secret pair in Azure keyvault
+# 5. So if you restart services they can pick up a new key/secret
+# 6. P.S. at some moment all the services need to be restarted (to pick up new keys) and old keys can be deleted from Confluent Cloud
--- src/main/java/io/confluent/uppercase/UppercaseTopology.java
+++ src/main/java/io/confluent/uppercase/UppercaseTopology.java
@@ -1,24 +1,14 @@
 package io.confluent.uppercase;
 
-import io.confluent.config.BasicAuthConfig;
-import io.confluent.config.SchemaRegistryConfig;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import io.confluent.model.avro.CountAndTotal;
 import io.confluent.model.avro.OnlineOrder;
-import org.apache.kafka.common.serialization.Serde;
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
-import org.springframework.lang.NonNull;
 
-import static io.confluent.common.Dictionary.COUNT_AND_TOTAL;
 import static io.confluent.common.Dictionary.ONLINE_ORDERS;
 
 @Configuration
@@ -26,27 +16,22 @@ import static io.confluent.common.Dictionary.ONLINE_ORDERS;
 public class UppercaseTopology {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UppercaseTopology.class);
+    private static final String ONLINE_ORDERS_UPPERCASE = ONLINE_ORDERS + "-uppercase";
 
     @Bean
-    public KStream<Object, Object> uppercase(StreamsBuilder streamsBuilder) {
+    public KStream<Object, OnlineOrder> uppercase(StreamsBuilder streamsBuilder) {
 
-        final KStream<Object, Object> onlineOrders = streamsBuilder
+        final KStream<Object, OnlineOrder> onlineOrders = streamsBuilder
                 .stream(ONLINE_ORDERS);
 
-        final KTable<String, CountAndTotal> aggregated =
-                onlineOrders
-                        .selectKey((m, n) -> n.getCustomerId() + "+" + n.getProductId())
-                        .repartition()
-                        .groupByKey()
-                        .aggregate(() -> new CountAndTotal(0, 0), (key, value, agg) -> {
-                            agg.setCount(agg.getCount() + 1);
-                            agg.setTotal(agg.getTotal() + value.getQuantity());
-                            return agg;
-                        }, mat);
+        onlineOrders
+                .mapValues(m -> {
+                    m.setProductId(m.getProductId());
+                    return m;
+                })
+                .peek((m, n) -> LOGGER.debug("Uppercase [" + m + ":" + n + "]"))
+                .to(ONLINE_ORDERS_UPPERCASE);
 
-        aggregated.toStream()
-                .peek((m, n) -> LOGGER.debug("Aggregated [" + m + ":" + n + "]"))
-                .to(COUNT_AND_TOTAL, Produced.with(STRING_SERDE, countAndTotalSerde));
 
         return onlineOrders;
     }
